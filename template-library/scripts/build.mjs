bcdbc78834bf2b73de13d946a0b6bfbde0bb3b21
Arman19941113 <td19941113@gmail.com>
2022-07-31 12:58:25 +0800

    chore: update configs

--- template-library/scripts/build.mjs
+++ template-library/scripts/build.mjs
@@ -11,7 +11,7 @@ process.on('exit', exitCode => {
 const run = (command) => execSync(command, { stdio: 'inherit' })
 
 await fs.remove('dist')
-run('tsc --project tsconfig.json')
+run('tsc --project tsconfig.json --outDir dist --declaration --emitDeclarationOnly')
 run('api-extractor run -c api-extractor.json')
 run('rollup -c rollup.config.mjs')
 await fs.remove('dist-ts')
